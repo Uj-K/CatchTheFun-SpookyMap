fa4a4563fb44f395322fc24ee1302e46e78dddc4
Uj-K <umkrall@gmail.com>
2025-07-20 23:02:18 -0700

    Update page to display Event Map with Google Maps

--- CatchTheFun.SpookyMap.Web/CatchTheFun.SpookyMap.Web/wwwroot/js/map.js
+++ CatchTheFun.SpookyMap.Web/CatchTheFun.SpookyMap.Web/wwwroot/js/map.js
@@ -1,1 +1,14 @@
-﻿
+﻿function initMap() {
+    const center = { lat: 47.2529, lng: -122.4443 }; // Tacoma
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
+        center: center,
+    });
+
+    new google.maps.Marker({
+        position: center,
+        map: map,
+        title: "🎃 Spooky Spot",
+    });
+}
