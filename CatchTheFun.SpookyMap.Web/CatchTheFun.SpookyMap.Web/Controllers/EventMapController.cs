1046f26d2a10bd1d8235d3949fc478f3fe7e8f81
Uj-K <umkrall@gmail.com>
2025-07-20 22:21:30 -0700

    Application structure for EventMap feature
    - Changed default `HomeController.cs` to `EventMapController.cs`
    - Updated routing in `Program.cs` to set `EventMap` as the default controller.
    - Removed `Privacy.cshtml`
    - Updated `_Layout.cshtml` with new branding and navigation links for EventMap.
    - Introduced a new JavaScript file, `map.js`.

--- CatchTheFun.SpookyMap.Web/CatchTheFun.SpookyMap.Web/Controllers/EventMapController.cs
+++ CatchTheFun.SpookyMap.Web/CatchTheFun.SpookyMap.Web/Controllers/EventMapController.cs
@@ -4,11 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CatchTheFun.SpookyMap.Web.Controllers
 {
-    public class HomeController : Controller
+    public class EventMapController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<EventMapController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public EventMapController(ILogger<EventMapController> logger)
         {
             _logger = logger;
         }
@@ -18,10 +18,6 @@ namespace CatchTheFun.SpookyMap.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
