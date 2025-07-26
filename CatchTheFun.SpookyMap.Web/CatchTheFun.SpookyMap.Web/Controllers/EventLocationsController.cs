797ab39d876e972366c4f1184bb7a3e60bc8b137
Uj-K <umkrall@gmail.com>
2025-07-26 12:03:16 -0700

    Add EventLocations CRUD functionality and views by using New Scaffolded Item - VC Controller with views, using Entity Framework

---
+++ CatchTheFun.SpookyMap.Web/CatchTheFun.SpookyMap.Web/Controllers/EventLocationsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CatchTheFun.SpookyMap.Web.Data;
+using CatchTheFun.SpookyMap.Web.Models;
+
+namespace CatchTheFun.SpookyMap.Web.Controllers
+{
+    public class EventLocationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventLocationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EventLocations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.EventLocations.ToListAsync());
+        }
+
+        // GET: EventLocations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventLocation = await _context.EventLocations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (eventLocation == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventLocation);
+        }
+
+        // GET: EventLocations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventLocations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,Description,EventType,Lat,Lng")] EventLocation eventLocation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(eventLocation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(eventLocation);
+        }
+
+        // GET: EventLocations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventLocation = await _context.EventLocations.FindAsync(id);
+            if (eventLocation == null)
+            {
+                return NotFound();
+            }
+            return View(eventLocation);
+        }
+
+        // POST: EventLocations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address,Description,EventType,Lat,Lng")] EventLocation eventLocation)
+        {
+            if (id != eventLocation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(eventLocation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventLocationExists(eventLocation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(eventLocation);
+        }
+
+        // GET: EventLocations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventLocation = await _context.EventLocations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (eventLocation == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventLocation);
+        }
+
+        // POST: EventLocations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var eventLocation = await _context.EventLocations.FindAsync(id);
+            if (eventLocation != null)
+            {
+                _context.EventLocations.Remove(eventLocation);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventLocationExists(int id)
+        {
+            return _context.EventLocations.Any(e => e.Id == id);
+        }
+    }
+}
