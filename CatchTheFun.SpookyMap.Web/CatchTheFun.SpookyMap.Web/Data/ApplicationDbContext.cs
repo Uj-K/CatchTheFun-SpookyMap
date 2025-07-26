ec6692089efe2b5f2aeb42b50b6181e87334391e
Uj-K <umkrall@gmail.com>
2025-07-26 11:46:52 -0700

    Commit before initial migration after Create model -Create EventLocation.cs in the models folder - ORM mapping by creating a data folder - add DbContext.cs -  update Program.cs - update appsettings.json

---
+++ CatchTheFun.SpookyMap.Web/CatchTheFun.SpookyMap.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CatchTheFun.SpookyMap.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CatchTheFun.SpookyMap.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EventLocation> EventLocations { get; set; }
+    }
+}
---
+++ CatchTheFun.SpookyMap.Web/CatchTheFun.SpookyMap.Web/Models/EventLocation.cs
@@ -0,0 +1,30 @@
+﻿// EventLocation 모델을 수정할 때마다 새로운 마이그레이션을 생성해야함
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CatchTheFun.SpookyMap.Web.Models
+{
+    public class EventLocation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Location Name")]
+        [StringLength(100, ErrorMessage = "Location name cannot exceed 100 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Display(Name = "Description (optional)")]
+        public string? Description { get; set; }
+
+        [Required]
+        [Display(Name = "Event Type")]
+        public string EventType { get; set; }
+
+        // Geocoding 결과로 저장되는 위치 정보
+        public double? Lat { get; set; }
+        public double? Lng { get; set; }
+    }
+}
